ba49abbe661026d945937d11d539ccba57a88554
xavier <lassus.xavier@gmail.com>
2020-11-25 23:36:10 +0100

    first init

---
+++ src/main/java/fr/allianz/bdai/sandbox/SandboxApplication.java
@@ -0,0 +1,13 @@
+package fr.allianz.bdai.sandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SandboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/allianz/bdai/sandbox/SandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.allianz.bdai.sandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
